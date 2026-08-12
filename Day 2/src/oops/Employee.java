abb8e08ef98e2b4291116b0d25e602d7463a0f1e
KRITI R <kritikumari140906@gmail.com>
2026-08-12 22:45:34 +0530

    Add Day 2 Java OOP programs

---
+++ Day 2/src/oops/Employee.java
@@ -0,0 +1,16 @@
+package oops;
+public class Employee {
+	int empId;
+	String empName;
+	int empAge;
+	String gender;
+	String email;
+	String dob;
+	long empPhoneNo;
+	String qualification;
+	String empType;
+	String dept;
+	String role;
+	int yearOfExperience;
+	double salary;
+}
---
+++ Day 2/src/oops/User.java
@@ -0,0 +1,21 @@
+package oops;
+public class User {
+	//data members or instance variables or properties
+	String userId;
+	String userName;
+	String password;
+	String email;
+	long phoneNo;
+	// data methods or behaviour
+	boolean isUsernameExists(String givenUsername) {
+		
+		return false;
+	}
+	void signup(String username,String password,String email,long mobile) {
+		
+	}
+	void deleteAccount(String username,String password) {
+		
+	}
+}
+
